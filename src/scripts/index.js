42d4263d04bd91b0e90d9682829a7fb9c3e42d15
alex.fan <alex.fan@okchem.com>
2019-05-07 16:42:15 +0800

    rte onchange

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,5 +7,16 @@ const showTa = function() {
 $(document).ready(function() {
   const ta1 = document.getElementById("ta1");
   ta1.addEventListener("change", showTa);
-  $("#rte1").summernote();
+  $("#rte1").summernote({
+    callbacks: {
+      onInit: function() {
+        console.log("Summernote is launched");
+      },
+      onChange: function(contents, $editable) {
+        const targetId = this.getAttribute("target");
+        const target = document.getElementById(targetId);
+        target.innerHTML = contents;
+      }
+    }
+  });
 });
