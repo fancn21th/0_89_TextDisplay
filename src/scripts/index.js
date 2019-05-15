acaf19bdcd2bf06a5c7861083d2451dca62ff9f7
alex.fan <alex.fan@okchem.com>
2019-05-15 12:04:41 +0800

    re-format content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,25 +7,24 @@ const showTa = function() {
 $(document).ready(function() {
   const ta1 = document.getElementById("ta1");
   ta1.addEventListener("change", showTa);
+  $("#rte1").on("summernote.change", function(we, contents, $editable) {
+    const pattern = "&nbsp;";
+    if (contents.includes(pattern)) {
+      we.preventDefault();
+      setTimeout(function() {
+        const reg = new RegExp("&nbsp;", "g");
+        const newText = contents.replace(reg, "");
+        $("#rte1").summernote("reset");
+        $("#rte1").summernote("pasteHTML", newText);
+      }, 100);
+    }
+  });
+
   $("#rte1").summernote({
     // https://summernote.org/deep-dive/#callbacks
     callbacks: {
       onInit: function() {
         console.log("Summernote is launched");
-      },
-      onChange: function(contents, $editable) {
-        const targetId = this.getAttribute("target");
-        const target = document.getElementById(targetId);
-        target.innerHTML = contents;
-      },
-      onPaste: function(e) {
-        var bufferText = (
-          (e.originalEvent || e).clipboardData || window.clipboardData
-        ).getData("Text");
-
-        e.preventDefault();
-
-        document.execCommand("insertText", false, bufferText);
       }
     }
   });
